26aa098d8a5de7268b59a7eafedaab0f407649d1
Mark <markhost@yandex.ru>
2017-06-23 23:16:53 +0300

    Пример кода обработчиков

--- wp-content/themes/zerotheme/index.php
+++ wp-content/themes/zerotheme/index.php
@@ -6,4 +6,17 @@
  * Time: 17:56
  */
 get_header();
+
+?>
+
+<a href="#" class="add-to-cart">add-to-cart</a><br />
+<a href="#" class="load-more">load-more</a>
+
+<form>
+    <div><input type="text" name="name" /></div>
+    <div><input type="submit" value="submit" /></div>
+</form>
+
+<?php
+
 get_footer();
--- wp-content/themes/zerotheme/js/script.js
+++ wp-content/themes/zerotheme/js/script.js
@@ -2,16 +2,46 @@
  * Created by mark on 13/06/17.
  */
 
-"use strict";
 
 (function ($) {
-    var $document = $(this);
 
-    var main = {
-        init: function () {
+    $(document).ready(function () {
 
-        }
-    };
+        var $document = $(this);
 
-    return main.init();
+        var main = {
+            init: function () {
+                var $main = this;
+                $(".add-to-cart").click(function (e) {
+                    console.log($main);
+                    e.preventDefault();
+                    $main.addToCart($(this));
+                    return true;
+                });
+                $(".load-more").click(function (e) {
+                    e.preventDefault();
+                    $main.loadMore($(this));
+                    return true;
+                });
+                $("form:not(.search)").submit(function (e) {
+                    e.preventDefault();
+                    $main.sendMessage($(this));
+                    return true;
+                });
+            },
+            addToCart: function ($obj) {
+                console.log($obj);
+            },
+            loadMore: function ($obj) {
+                console.log($obj);
+            },
+            sendMessage: function ($obj) {
+                console.log($obj);
+            }
+        };
+
+        main.init();
+
+    });
+    
 })(jQuery);
