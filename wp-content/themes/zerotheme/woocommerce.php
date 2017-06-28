b560afc5b09f3356d05c474b4593beacef17d04d
Mark <markhost@yandex.ru>
2017-06-28 22:14:50 +0300

    Проба верстки под вуу

--- wp-content/themes/zerotheme/woocommerce.php
+++ wp-content/themes/zerotheme/woocommerce.php
@@ -7,6 +7,21 @@
  */
 get_header();
 
-woocommerce_content();
+?>
+
+<div class="container-fluid">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <?php woocommerce_content(); ?>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<?php
 
 get_footer();
--- wp-content/themes/zerotheme/woocommerce/content-product.php
+++ wp-content/themes/zerotheme/woocommerce/content-product.php
@@ -27,7 +27,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	return;
 }
 ?>
-<li <?php post_class(); ?>>
+<li <?php post_class( 'col-md-4' ); ?>>
 	<?php
 	/**
 	 * woocommerce_before_shop_loop_item hook.
